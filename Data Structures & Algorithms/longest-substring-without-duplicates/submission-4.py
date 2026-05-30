ed6260ae07d61ae6dc086dc8361084ab3e4b64a3
Aayush Dineshkumar Jain <50103773+Darkshadow9799@users.noreply.github.com>
2026-05-30 18:19:33 +0530

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longestString = ""
+        local_max_string = ""
+        for  i in range(len(s)):
+            if s[i] in local_max_string:
+                if len(longestString) < len(local_max_string):
+                    longestString = local_max_string
+                local_max_string = local_max_string[local_max_string.find(s[i]) + 1:] + s[i]
+            else:
+                local_max_string += s[i]
+        # print(local_max_string)
+        # print(longestString)
+        if len(longestString) < len(local_max_string):
+            longestString = local_max_string
+
+        return len(longestString)
